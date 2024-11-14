8a54300b977d68e14a80427970022ae000a3c6a6
sabekunnaharbably <bablysunflower@gmail.com>
2024-11-14 22:33:19 +0600

    Add hello

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,7 @@ import Container from './components/Container'
 
 function App() {
 
-  let dropRef = useRef(null)
-
-  let handleDrop = () => {
-
-    // console.log(dropRef.current);
-
-    // dropRef.current.style.display ="block"
-
-    if (dropRef.current.style.display == "block") {
-      dropRef.current.style.display = "none"
-    }
-    
-    else{
-      dropRef.current.style.display ="block"
-    }
-
-  }
-
-
+  
 
 
 
@@ -33,17 +15,7 @@ function App() {
   return (
     <>
       <Container className={"max-w-headerContainer"}>
-        <div onClick={handleDrop}>
-          <button className={"bg-orange-300 py-4 px-6 rounded-lg text-white mt-4 mb-4"}>Click</button>
-          <div className="bg-emerald-300 p-6 hidden" ref={dropRef}>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-              <li>Service</li>
-            </ul>
-          </div>
-        </div>
+    <h1>hello</h1>
       </Container>
 
 
